283a60febee568336aa9159653e3ab892fc1dc76
Kimbers <fraser.kimberleym@gmail.com>
2021-04-22 11:37:55 -0400

    Gut start module

--- gcit-start/src/main/java/com/example/gcit_start/CountryRecyclerViewAdapter.kt
+++ gcit-start/src/main/java/com/example/gcit_start/CountryRecyclerViewAdapter.kt
@@ -1,42 +1,19 @@
 package com.example.gcit_start
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 
-class CountryRecyclerViewAdapter(private val countryList: List<Country>, private val countryDataInterface: CountryDataInterface) : RecyclerView.Adapter<CountryRecyclerViewAdapter.ViewHolder>() {
-
-
-    inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
-        val countryNameTextView: TextView = itemView.findViewById(R.id.country_name)
+class CountryRecyclerViewAdapter(private val countryList: List<Country>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
     }
 
-    interface CountryDataInterface {
-        fun getSelectedCountry(country: Country)
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val context: Context = parent.context
-        val inflater = LayoutInflater.from(context)
-
-        // Inflate the custom layout
-        val contactView: View = inflater.inflate(R.layout.country_item, parent, false)
-
-        // Return a new holder instance
-        return ViewHolder(contactView)
-    }
-
-    override fun getItemCount(): Int = countryList.size
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val country: Country = countryList[position]
-        viewHolder.countryNameTextView.text = country.getDisplayName()
-        viewHolder.countryNameTextView.setOnClickListener{
-            countryDataInterface.getSelectedCountry(country)
-        }
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 
 
--- gcit-start/src/main/java/com/example/gcit_start/DatePickerDialogFragment.kt
+++ gcit-start/src/main/java/com/example/gcit_start/DatePickerDialogFragment.kt
@@ -11,35 +11,9 @@ import java.util.*
 
 
 class DatePickerDialogFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
-
-    var listener: DatePickerInterface? = null
-
-    interface DatePickerInterface {
-        fun getSelectedDate(year: Int, month: Int, day: Int)
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        TODO("Not yet implemented")
     }
 
 
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        listener = context as DatePickerInterface
-    }
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val calendar: Calendar = Calendar.getInstance()
-        val year: Int = calendar.get(Calendar.YEAR)
-        val month: Int = calendar.get(Calendar.MONTH)
-        val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
-
-        val dpd = DatePickerDialog(activity,
-                AlertDialog.THEME_HOLO_LIGHT, this, year, month, day)
-
-        dpd.setTitle("Please select a date")
-
-        dpd.datePicker.maxDate = System.currentTimeMillis();
-
-        // Return the DatePickerDialog
-        return dpd
-    }
-    override fun onDateSet(datePicker: DatePicker?, year: Int, month: Int, day: Int) {
-        listener?.getSelectedDate(year, month, day)
-    }
 }
